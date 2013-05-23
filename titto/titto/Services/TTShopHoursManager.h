81615019bf4f2b883d1d6a613e551a8ed6200f49
Paolo Ladisa <paolo@paololadisa.com>
2013-05-23 22:10:58 +0200

    fix

---
+++ titto/titto/Services/TTShopHoursManager.h
@@ -0,0 +1,13 @@
+//
+//  TTShopHoursManager.h
+//  titto
+//
+//  Created by Paolo Ladisa on 5/22/13.
+//  Copyright (c) 2013 titto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TTShopHoursManager : NSObject
+
+@end
---
+++ titto/titto/Services/TTShopHoursManager.m
@@ -0,0 +1,13 @@
+//
+//  TTShopHoursManager.m
+//  titto
+//
+//  Created by Paolo Ladisa on 5/22/13.
+//  Copyright (c) 2013 titto. All rights reserved.
+//
+
+#import "TTShopHoursManager.h"
+
+@implementation TTShopHoursManager
+
+@end
